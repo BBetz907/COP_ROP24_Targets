636d9de8221c5c5068c749140c33d1fbbda4a458
Bourke Betz <64644141+BBetz907@users.noreply.github.com>
2024-03-11 21:46:07 -0700

    updating scripts

--- Scripts/2b pre-psnu x im.R
+++ Scripts/2b pre-psnu x im.R
@@ -1,7 +1,9 @@
 tasts <- tast_filepaths %>% map_dfr(tame_dp, map_names = FALSE,
                                # psnu_lvl = TRUE,
                                # type = "PSNUxIM"
-                               ) %>% filter(!is.na(targets)) %>% 
+                               ) %>% filter(!is.na(targets),
+                                            # fiscal_year > 2024
+                                            ) %>% 
   mutate(source_type = "TaST",
          operatingunit = if_else(country %in% c("Central America and Brazil", "Caribbean Region"), "Western Hemisphere Region", operatingunit),
          country = if_else(country %in% c("Central America and Brazil", "Caribbean Region") & str_detect(psnu, countrynames), str_extract(psnu, countrynames), 
@@ -9,3 +11,4 @@ tasts <- tast_filepaths %>% map_dfr(tame_dp, map_names = FALSE,
 table(tasts$operatingunit)
 table(tasts$country)
 
+tasts |> filter(fiscal_year==2023) |> count(operatingunit)
